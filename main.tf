a1416def01bff035f3e71d528d892c291ddd6c36
Marco Aloisi <Marco.Aloisi@baxenergy.com>
2023-11-27 18:35:21 +0100

    add delete disk vm

--- main.tf
+++ main.tf
@@ -56,10 +56,10 @@ resource "azurerm_virtual_machine" "main" {
   vm_size               = "Standard_DS1_v2"
 
   # Uncomment this line to delete the OS disk automatically when deleting the VM
-  # delete_os_disk_on_termination = true
+  delete_os_disk_on_termination = true
 
   # Uncomment this line to delete the data disks automatically when deleting the VM
-  # delete_data_disks_on_termination = true
+  delete_data_disks_on_termination = true
 
   storage_image_reference {
     publisher = "Canonical"
@@ -70,7 +70,7 @@ resource "azurerm_virtual_machine" "main" {
   storage_os_disk {
     name              = "myosdisk1"
     caching           = "ReadWrite"
-    create_option     = "FromImage"
+    create_option     = ""
     managed_disk_type = "Standard_LRS"
   }
   os_profile {
